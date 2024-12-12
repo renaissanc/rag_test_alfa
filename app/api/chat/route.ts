a964d78928d54fe4776c245e180893936d7a9422
renaissanc <40231425+renaissanc@users.noreply.github.com>
2024-12-12 13:48:37 +0100

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -71,7 +71,7 @@ export async function POST(req: Request) {
         };
 
         const response = await openai.chat.completions.create({
-            model: "gpt-4",
+            model: "gpt-4o",
             stream: true,
             messages: [template, ...messages],
         });
