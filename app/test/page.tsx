d416eb9d44c21e2326f894fa76aebc5b86dae00e
renaissanc <40231425+renaissanc@users.noreply.github.com>
2024-12-26 16:48:10 +0100

    Update page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,6 +5,346 @@ const App: React.FC = () => {
     <div>
       <h1>Hello, World!</h1>
       <p>This is HTML syntax in a TypeScript file.</p>
+      <p>
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        20
+        24
+        BUSINESS
+        INTELLIGENCE
+        Part I
+        Making Sense out of Data
+        BUSINESS
+        INTELLIGENCE
+        I
+        Winter
+        Term
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        PERSONAL DETAILS
+        Instructor
+        0 // Business Intelligence I / Introduction 2
+        Education:
+         Studies in Business Information Systems (Wirtschaftsinformatik), University of Bamberg
+         Research and Teaching Assistant, Chair of Systems Engineering and Database Applications
+         Dissertation: Data Analysis Processes
+        Profession:
+         since 2019: Professor for Business Intelligence, THWS
+         since 2005: Freelance Information Systems Consultant
+        Core competences:
+         Data analytics, process optimisation, systems architectures, requirements engineering
+        Industry Focus:
+         Retail, consumer goods, IT services, smart farming, market research
+        Prof. Dr. rer. pol. Bernd Knobloch
+        Diplom-Wirtschaftsinformatiker Univ.
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        PERSONAL DETAILS
+        Participants
+        0 // Business Intelligence I / Introduction 3
+        Please give a short introduction of yourself.
+         Name, semester, home country
+         Do you already have experience with Business Intelligence or data analysis
+        (e.g., internships or other contact points in private or business)?
+         Which tools are you familiar with?
+        (e.g., Excel, SQL, PowerBI, Tableau, Qlik, SPSS, others?)
+         What is your idea of Business Intelligence?
+        Who are you?
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        4
+        ORGANISATIONAL MATTERS
+        0 // Business Intelligence I / Introduction
+         E-Learning: WS 24/25: Business Intelligence I (BI1E)
+        Key: Bint2425
+         13 weeks = 26 course units
+         2 sessions per week
+         Lecture + hands-on exercises
+         Examination: written exam
+         E-Mail: bernd.knobloch@thws.de
+         Office: 20.1.96 (Campus Ledward)
+         Consultation hours: Wednesday, 1030-1130 hrs
+        Please send me an e-mail in advance!
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        5
+        WHAT MATTERS TO ME
+        Course Objectives
+        0 // Business Intelligence I / Introduction
+        [colourbox.de]
+        Generally:
+        Impart sustainable knowledge.
+        Encourage judgment powers and creativity.
+        Think out of the box.
+        Business Intelligence:
+        You understand the foundations of Business Intelligence.
+        You can assess and evaluate BI architectures and tools.
+        You are able to plan, design, and implement BI processes and systems.
+        You understand the relationships between business aspects and data.
+        You can select and model data and use them to analyse business situations.
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        6
+        WHAT ARE YOUR EXPECTATIONS ON THIS COURSE?
+        Course Objectives
+        0 // Business Intelligence I / Introduction
+        [colourbox.de]
+        What do you expect from this course in particular
+        (and from Digital Business in gerneral)?
+        Which knowledge and skills would you like to
+        take away from this course?
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        7
+        STRUCTURE OF THE COURSE
+        What are the Two Parts About?
+        0 // Business Intelligence I / Introduction
+        Two Consecutive Modules:
+        Business Intelligence I
+        Making Sense out of Data
+        EN: Winter Term | DE: Summer Term
+        Focus: Structured Data
+        Foundations and Principles of Business Intelligence
+        Processes and Tools for Extracting, Transforming, and Analysing Data
+        Business Intelligence II
+        Coping with Big Data
+        EN: Summer Term | DE: Winter Term
+        Focus: Big Data – High Volumes, high Velocity, high Variety
+        Advanced Solutions and Architectures for Analysing Data
+        Principles and Platforms for Storing, Processing, and Analysing Big Data
+        After that, you‘ll be a BI and Analytics Expert! 
+        Time
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        8
+        JOB OPPORTUNITIES FOR BI SPECIALISTS
+        How Much Can I Expect when I Specialise in BI & Analytics?
+        0 // Business Intelligence I / Introduction
+        Data Analytics Salaries in 2024 (DACH): [BARC 2024]
+        Mean salaries:
+        by professional experience: by education level:
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        9
+        JOB OPPORTUNITIES FOR BI SPECIALISTS
+        How Much Can I Expect when I Specialise in BI & Analytics?
+        0 // Business Intelligence I / Introduction
+        Some Random Job Offerings I found in my Mailbox last week:
+        [colourbox.de]
+        Prof. Dr. Bernd Knobloch Overview Business Intelligence
+        10
+        BUSINESS INTELLIGENCE I
+        1. The Idea Behind Business Intelligence
+        Motivation – Terms and Concepts – What BI is (and what it is not)
+        2. Architectures and Layers for BI
+        Management Information Systems – Functional and Technical Architectures
+        3. Data Integration: ETL and Beyond
+        Ingesting Data –Transforming and Cleansing Data – Levels of Detail
+        4. Data Storage: Data Warehouses, Marts, and Lakes
+        Single Point of Truth – Self-Service for Decision Makers – Modelling Information
+        5. Data Analysis: Reporting, Mining, and Visualisation
+        Recipients, Analysts, and Decision Makers – Presentation and Communication
+        Business Intelligence / Overview
+        Prof. Dr. Bernd Knobloch Overview Business Intelligence
+        11
+        BUSINESS INTELLIGENCE II
+        6. Big Data: Challenges and Solution Options
+        More, Faster, Different – Advanced Architectures – Agile Analytics
+        7. Business Intelligence Tools and Platforms
+        Defining Requirements – Selecting Software – Sourcing and Evolving
+        8. Machine Learning Applications
+        Feature Engineering – Model Fitting – Evaluation and Application
+        9. Requirements Analysis and Analytical Projects
+        From Business Questions to Analytical Answers – How not to lose control
+        10. Strategy, Organisation, and Governance
+        Roadmaps – Responsibilities – Teams – Processes – Privacy
+        Business Intelligence / Overview
+        Prof. Dr. Bernd Knobloch Overview Business Intelligence
+        12
+        BUSINESS INTELLIGENCE (I + II)
+        1. The Idea Behind Business Intelligence
+        2. Architectures and Layers for BI
+        3. Data Integration: ETL and Beyond
+        4. Data Storage: Data Warehouses, Marts, and Lakes
+        5. Data Analysis: Reporting, Mining, and Visualisation
+        6. Business Intelligence Tools and Platforms
+        7. Machine Learning Applications
+        8. Data and Information Quality
+        9. Strategy, Organisation, and Governance
+        10. Trends, Limitations, and Challenges
+        Business Intelligence / Overview
+      </p>
+      <p>Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        20
+        24
+        BUSINESS
+        INTELLIGENCE
+        Part I
+        Making Sense out of Data
+        BUSINESS
+        INTELLIGENCE
+        I
+        Winter
+        Term
+        Prof. Dr. Bernd Knobloch Overview Business Intelligence
+        2
+        BUSINESS INTELLIGENCE I
+        1. The Idea Behind Business Intelligence
+        2. Architectures and Layers for BI
+        3. Data Integration: ETL and Beyond
+        4. Data Storage: Data Warehouses, Marts, and Lakes
+        5. Data Analysis: Reporting, Mining, and Visualisation
+        Business Intelligence I / Overview
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        THE IDEA BEHIND
+        BUSINESS INTELLIGENCE
+        Chapter 1: What is it all about? 1
+        BUSINESS
+        INTELLIGENCE
+        I
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        4
+        BUSINESS INTUITION
+        How do business leaders make decisions – traditionally?
+        1 // Business Intelligence I / The Idea Behind Business Intelligence
+        Intuition:
+         Intuition is an elementary human competence, enabling us to process information
+        and to initiate suitable reactions in situations with a high rate of complexity.
+         „It appears that intuition can be said to occur when an individual reaches a
+        conclusion on the basis of less explicit information than is ordinarily required to
+        reach that conclusion.“
+        – Westcott (1968), p. 97
+        [Bachmann/Kemper 2011, p. 25]
+        ”Business Intuition” is the successful
+        use of intuition as a management tool
+        to make business decisions.
+         Intuitive decisions may trump rational
+        decisions in complex situations.
+         However:
+         No explanation available.
+         Decisions remain highly subjective.
+        Business Intuition may be helpful, when
+         Decisions are pressed for time
+         Decision maker has sound experience
+         Risk of misperception is low
+         Intuition is supported by additional
+        information that can be processed
+         Use data, when they are available!
+        [Bachmann/Kemper 2011, pp. 24ff.]
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        5
+        DATA-DRIVEN ENTERPRISE
+        Why should we rely on data?
+        1 // Business Intelligence I / The Idea Behind Business Intelligence
+        Was ist Datenanalyse?
+        „It is a capital mistake to theorisebefore one has data.“ – Sherlock Holmes
+        [colourbox.de]
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        6
+        DATA-DRIVEN ENTERPRISE
+        Which role do data play in Digital Business?
+        1 // Business Intelligence I / The Idea Behind Business Intelligence
+        Data are an Asset
+         Information as an asset is a competitive differentiator for leading
+        organisations as they focus on digital transformation.
+         Data and analytics become strategic priorities.
+         A company’s ability to compete in the emerging digital economy will
+        require faster-paced, forward-looking decisions.
+         Leading organisations in every industry are wielding data and analytics as
+        competitive weapons, operational accelerants and innovation catalysts.
+         Organisations will move away from merely using data as a resource and
+        analytics as reporting and decision-making support tools.
+         Data and analytics will become the centerpiece of enterprise strategy,
+        focus and investment.
+        [https://www.gartner.com/smarterwithgartner/why-data-and-analytics-are-key-to-digital-transformation/, March 2019]
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        7
+        DATA-DRIVEN ENTERPRISE
+        Why is the usage of available data an imperative?
+        1 // Business Intelligence I / The Idea Behind Business Intelligence
+        [colourbox.de]
+        „We may pardon those who were defeated,
+        but never those who were surprised.“ – Frederick the Great
+        King of Prussia
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        8
+        DATA-DRIVEN ENTERPRISE
+        Does the strategic role of data make a difference?
+        1 // Business Intelligence I / The Idea Behind Business Intelligence
+        The Rationale for Becoming a Data-Driven Enterprise
+         Making data-based decisions makes instinctive and commercial sense:
+         Organisations need to ask themselves why they need data (today)?
+         For most organisations, this desire will be to better understand one’s customers and to
+        improve products and services. (Ernst & Young)
+         Most companies have more than enough data to make insightful decisions, but the data
+        are poorly managed and exploited.
+         Information needs to be delivered locally to ensure that insights are available to
+        employees when they need it.
+        [https://www.forbes.com/sites/adigaskell/2016/10/28/becoming-a-data-driven-organization, October 2016]
+        Data-driven organisations are
+        • 23 times more likely to acquire customers;
+        • 6 times more likely to retain those customers; and
+        • 19 times more likely to be profitable as a result.
+        – McKinsey Global Institute
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        9
+        DATA-DRIVEN ENTERPRISE
+        Why do we need to change the way we use data?
+        1 // Business Intelligence I / The Idea Behind Business Intelligence
+        We are Drowning in Data, but Starving for Information…
+        [Reinnarth 2019, pp. 8-11]
+        „If informationistoserveasa basisfordecision making, thentheyneedtobeaccessible
+        and compilablein reliable qualityat regular intervals. In practice, thingsoftenlookdifferently.
+        Let‘saska simple question:
+        ‚How manynew customersdo weacquireper month?‘
+        In onecompany, wehavetriedtogetthisquestionansweredforovera year.“
+        – Jörg Reinnarth
+        [colourbox.de]
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        10
+        DATA-DRIVEN ENTERPRISE
+        What should be done?
+        1 // Business Intelligence I / The Idea Behind Business Intelligence
+        [https://www.gartner.com/smarterwithgartner/build-a-data-driven-organization, September 2018]
+        Building the Data-Driven Enterprise
+        • A digital business cannot exist without data and analytics.
+        • Data and their analysis bear great potential for valuable insights.
+        • Businesses have to identify data sources, data quality issues, types of analysis
+        and new work practices needed to use those insights.
+        • Data-driven opportunities often cross organisational boundaries and require
+        operating model changes to achieve the intended outcomes.
+        • Data-driven ambitions change the nature of the business.
+        • Data and analytics must be woven into everything.
+        • Establish a data-driven culture!
+        • The entire organisation needs to become data-literate.
+        • Being data-driven is a team sport and requires collaboration, as well as effective
+        and ongoing training programs.
+        – McKinsey Global Institute
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        11
+        BUSINESS INTELLIGENCE
+        What‘s the basic idea?
+        1 // Business Intelligence I / The Idea Behind Business Intelligence
+        The Spider in the Web
+        Being aware of everything that’s going on in your reach – just like the spider in the web.
+        [colourbox.de]
+        Prof. Dr. Bernd Knobloch
+        Business Intelligence
+        12
+        BUSINESS INTELLIGENCE
+        What is Business Intelligence?
+        1 // Business Intelligence I / The Idea Behind Business Intelligence
+      </p>
     </div>
   );
 };
