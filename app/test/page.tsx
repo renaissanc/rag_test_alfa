6aa2a1cd56386a36e4471cb714d0cd2610a225db
renaissanc <40231425+renaissanc@users.noreply.github.com>
2024-12-26 16:29:37 +0100

    Update page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,1 +1,6 @@
 
+export default function page(){
+    return (
+        <div>THWS GPT was created by a skilled data analyst and a dedicated, talented computer scientist and physicist, Murtaza ali, who aspires to make a meaningful impact in the business world.</div>
+    )
+}
